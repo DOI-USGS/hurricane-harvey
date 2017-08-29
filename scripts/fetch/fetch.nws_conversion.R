302538fb0c14dc16701c53c79679a62dfb2d3a8c
Laura DeCicco <ldecicco@usgs.gov>
2017-08-29 15:20:52 -0500

    Less site filter

--- scripts/fetch/fetch.nws_conversion.R
+++ scripts/fetch/fetch.nws_conversion.R
@@ -1,11 +1,12 @@
 library(readr)
 
-fetch.nws_conversion <- function(viz){
+fetch.nws_conversion <- function(viz = as.viz("nws_conversion")){
   conversion.url <- "http://www.nws.noaa.gov/oh/hads/USGS/TX_USGS-HADS_SITES.txt"
   conversion.table <- read_delim(conversion.url,
                                  delim = "|",skip = 4,col_names = FALSE)
 
   names(conversion.table) <- c("NWS","USGS","GOES","NWS HSA","lat","lon","name")
+  conversion.table$USGS <- gsub(" ","", conversion.table$USGS)
   
   location <- viz[['location']]
   saveRDS(conversion.table, file=location)
