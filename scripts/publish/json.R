1dbd3d54495987394080da1d999f59860b8e4480
Laura DeCicco <ldecicco@usgs.gov>
2017-08-30 11:59:04 -0500

    I think this will get the right mimetype on the json

---
+++ scripts/publish/json.R
@@ -0,0 +1,12 @@
+publish.json <- function(viz) {
+  required <- c("relpath", "mimetype")
+
+  checkRequired(viz, required)
+  
+  output <- NULL
+  if (!is.na(viz[['relpath']])) {
+    output <- sprintf('<script src="%s?_c=%s" type="text/javascript"></script>',
+                      viz[['relpath']],runif(1, 10000, 10000000))
+  }
+  return(output)
+}
