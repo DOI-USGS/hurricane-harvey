628c6dcf99065b70125e783caa3ce5d845c8b224
Jordan S Read <jread@usgs.gov>
2017-09-05 16:30:44 -0500

    adding documentation to functions

--- scripts/visualize/visualize_utils.R
+++ scripts/visualize/visualize_utils.R
@@ -10,6 +10,11 @@ visualize.svg_base_map <- function(viz){
   # 5) loop through depends, trim, then add geoms to <use/> elements (in id="template-geoms"), with id="u-{id}"
   # 6) create geoms to mirror ids in <use/> elements, add attributes
 }
+
+#' calculate the bounding box of the sp object and return as `SpatialPolygons` object
+#' @param sp a spatial object
+#' 
+#' @return a `SpatialPolygons` object that represents the bounding box of the input `sp`
 get_sp_bbox <- function(sp){
   bb <- bbox(sp)
   Sr1 <- Polygon(cbind(c(bb[c(1, 3, 3, 1, 1)]), c(bb[c(2, 2, 4, 4, 2)])))
@@ -18,6 +23,18 @@ get_sp_bbox <- function(sp){
   return(SpP)
 }
 
+
+#' extract the svg elements from an sp object
+#' 
+#' @param sp a spatial object
+#' @param ... additional arguments passed to the plotting methods of `sp` (e.g., `expandBB`)
+#' @param width the width (in inches) of the svg view
+#' @param height the height (in inches) of the svg view
+#' @param pointsize number of pixels per inch
+#' @param xlim x limits (in sp units) of the plot
+#' @param ylim y limits (in sp units) of the plot
+#' 
+#' @return an `xml_document`
 get_svg_geoms <- function(sp, ..., width = 10, height = 8, pointsize = 12, xlim, ylim){
   
   stopifnot(packageVersion('svglite') == '1.2.0.9002')
@@ -29,17 +46,34 @@ get_svg_geoms <- function(sp, ..., width = 10, height = 8, pointsize = 12, xlim,
     ylim <- get_sp_lims(sp, ..., return = 'ylim')
   }
   
-  geoms <- xml2::xml_new_document()
+  # clip the spatial object so that it only contains features and data that are within the plotting range:
+  clipped.sp <- clip_sp(sp, xlim, ylim)
   
+  # establish the holder of the geoms as an xml doc
+  geoms <- xml2::xml_new_document()
   
   rendered <- svglite::xmlSVG(width = width, height = height, pointsize = pointsize, standalone = F, {
     set_sp_plot()
-    plot(sp, ..., xlim = xlim, ylim = ylim)
+    plot(clipped.sp, ..., xlim = xlim, ylim = ylim)
   })
   
+  sp.geoms <- tail(xml2::xml_children(rendered), length(sp))
   
-  return(tail(xml_children(rendered), length(sp))) # removing the <rect/> element...
+  # here either strip the important attributes out and re-add them with a xml_set_attrs call, or lapply the nodeset and add attrs one by one:
+  
+  return(sp.geoms) # removing the <rect/> element...
 }
+
+#' extract the plotting limits from a spatial object, given a sized svg view
+#' 
+#' @param sp a spatial object
+#' @param ... additional arguments passed to the plotting methods of `sp` (e.g., `expandBB`)
+#' @param width the width (in inches) of the svg view
+#' @param height the height (in inches) of the svg view
+#' @param pointsize number of pixels per inch
+#' @param return what limits to return
+#' 
+#' @return a list or numeric vector, depending on what is specified for `return`
 
 get_sp_lims <- function(sp, ..., width = 10, height = 8, pointsize = 12, return = c('xlim','ylim')){
   bb <- get_sp_bbox(sp)
@@ -62,10 +96,21 @@ get_sp_lims <- function(sp, ..., width = 10, height = 8, pointsize = 12, return 
   
 }
 
-clip_sp <- function(sp, clip.box){
-  message('not implemented yet')
+#' retain data and features for all polys/points within the plotting range,
+#' while removing those outsite and truncating the boundaries of 
+#' polygons to the bounds of the plotting range. 
+#' 
+#' @param sp a spatial object
+#' @param xlim the x limits of the plot using the same coordinate system as `sp`
+#' @param ylim the y limits of the plot using the same coordinate system as `sp`
+#' 
+#' @return a clipped sp object
+clip_sp <- function(sp, xlim, ylim){
+  message('clip_sp is not implemented yet')
+  return(sp)
 }
 
+#' set up the basic plot par for a map
 set_sp_plot <- function(){
   par(mai=c(0,0,0,0), omi=c(0,0,0,0), xaxs = 'i', yaxs = 'i')
 }
